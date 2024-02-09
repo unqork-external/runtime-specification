ddc7e51b4fa3a1559ab7696b687d2a467c61da0a
unqork-external <>
2024-02-09 16:19:01 +0000

    Update from https://github.com/unqork/runtime/commit/7e5150e4c9caccea4538e737f1320d72b078c4b3

--- components/iFrame/iFrameOptions.ts
+++ components/iFrame/iFrameOptions.ts
@@ -1,6 +1,17 @@
 import { Description, Optional, Required } from '@tsed/schema'
 
+export type IFrameOptionsSandbox =
+  | 'allow-forms'
+  | 'allow-modals'
+  | 'allow-popups'
+  | 'allow-same-origin'
+  | 'allow-scripts'
+
 export class IFrameOptions {
+  @Required()
+  @Description('The source URL displayed in the IFrame')
+  src: string
+
   @Optional()
   @Description('The width of the IFrame in pixels')
   width?: number
@@ -9,7 +20,7 @@ export class IFrameOptions {
   @Description('The height of the IFrame in pixels')
   height?: number
 
-  @Required()
-  @Description('The source URL displayed in the IFrame')
-  src: string
+  @Optional()
+  @Description('The "sandbox" setting of the IFrame')
+  sandbox?: Array<IFrameOptionsSandbox>
 }
